abd40d1e6e67b76c852f5b5a63e721403f311428
sbx_user1051 <sbx_user1051@169.254.98.85>
2025-06-14 10:08:39 +0000

    Sat Jun 14 10:08:39 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,6 +3,22 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
+        prices = {
+            'A':50,
+            'B':30,
+            'C':20,
+            'D':15,
+        }
+        offers = {
+            'A': (3,130)
+            ,'B': (2,45),
+        }
+
+        for sku in skus:
+            if sku not in prices:
+                return -1
+
         
+
 
 
