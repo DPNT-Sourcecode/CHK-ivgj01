63555568017c8c7bd217f982ec65915fe9f7aeb8
sbx_user1051 <sbx_user1051@169.254.98.85>
2025-06-14 10:14:39 +0000

    Sat Jun 14 10:14:39 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -18,7 +18,24 @@ class CheckoutSolution:
             if sku not in prices:
                 return -1
 
-        
+        A_count=0
+        B_count=0
+        C_count=0
+        D_count=0
+
+        total = 0
+        for sku in skus:
+            if sku =='A':
+                A_count+=1
+            elif sku=='B':
+                B_count+=1
+            elif sku=='C':
+                C_count+=1
+            elif sku=='D':
+                D_count+=1
+
+        A_count/
+
 
 
 
